39f538884d0ec7077b2beb5022137b29241a90a8
jericacilo <eric.acilo@live.com>
2026-03-28 03:34:56 -0600

    user java source code updated as per my duties

--- src/sait/sll/problemdomain/User.java
+++ src/sait/sll/problemdomain/User.java
@@ -1,22 +1,23 @@
 package sait.sll.problemdomain;
 
+import java.io.Serializable;
+
 /**
  * Represents a user.
- * @version 2025
  *
+ * @version 2025
  */
-public class User {
-	
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	private int id;
-	
 	private String name;
-	
 	private String email;
-	
-	private String password;
-	
+	private transient String password;
+
 	/**
 	 * Creates a User object with specified arguments.
+	 *
 	 * @param id user ID.
 	 * @param name user name.
 	 * @param email user e-mail address.
@@ -28,56 +29,132 @@ public class User {
 		this.email = email;
 		this.password = password;
 	}
-	
+
 	/**
 	 * Gets the user ID.
+	 *
 	 * @return user ID.
 	 */
 	public int getId() {
 		return this.id;
 	}
-	
+
+	/**
+	 * Sets the user ID.
+	 *
+	 * @param id new user ID.
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	/**
 	 * Gets the user name.
+	 *
 	 * @return user name.
 	 */
 	public String getName() {
 		return this.name;
 	}
-	
+
+	/**
+	 * Sets the user name.
+	 *
+	 * @param name new user name.
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	/**
 	 * Gets the user email address.
+	 *
 	 * @return e-mail address.
 	 */
 	public String getEmail() {
 		return this.email;
 	}
-	
+
+	/**
+	 * Sets the user email address.
+	 *
+	 * @param email new e-mail address.
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * Gets the user password.
+	 *
+	 * @return password.
+	 */
+	public String getPassword() {
+		return this.password;
+	}
+
+	/**
+	 * Sets the user password.
+	 *
+	 * @param password new password.
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	/**
 	 * Checks if the passed password is correct.
+	 *
 	 * @param password password to check.
 	 * @return true if password is correct, false otherwise.
 	 */
 	public boolean isCorrectPassword(String password) {
-		if (this.password == null && password == null)
+		if (this.password == null && password == null) {
 			return true;
-		else if (this.password == null || password == null)
+		}
+		if (this.password == null || password == null) {
 			return false;
-		else
-			return this.password.equals(password);
+		}
+		return this.password.equals(password);
 	}
-	
+
 	/**
 	 * Checks if object is equal to another object.
+	 *
 	 * @param obj the object to compare to.
 	 * @return true if the objects are equal, false otherwise.
 	 */
+	@Override
 	public boolean equals(Object obj) {
-		if (!(obj instanceof User))
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof User)) {
 			return false;
-		
+		}
+
 		User other = (User) obj;
-		
-		return this.id == other.id && this.name.equals(other.name) && this.email.equals(other.email);
+		return this.id == other.id
+				&& stringEquals(this.name, other.name)
+				&& stringEquals(this.email, other.email);
+	}
+
+	@Override
+	public int hashCode() {
+		int result = 17;
+		result = 31 * result + this.id;
+		result = 31 * result + (this.name == null ? 0 : this.name.hashCode());
+		result = 31 * result + (this.email == null ? 0 : this.email.hashCode());
+		return result;
+	}
+
+	private boolean stringEquals(String first, String second) {
+		if (first == second) {
+			return true;
+		}
+		if (first == null || second == null) {
+			return false;
+		}
+		return first.equals(second);
 	}
 }
